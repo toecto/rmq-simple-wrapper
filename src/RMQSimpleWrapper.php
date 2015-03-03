c5c5abb43832e8dc7acfe102d06385f6366dea5d
toecto <maxim@zalil.ru>
2015-03-03 00:10:36 -0500

    added composer and wrapper class

---
+++ src/RMQSimpleWrapper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace toecto\AMQPSimpleWrapper;
+
+class RMQSimpleWrapper {
+
+    protected $connection;
+
+    public function __construct($host, $user, $password, $vhost, $port) {
+
+    }
+
+    public function publish($key, $message) {
+
+    }
+
+    public function createExchange($name) {
+        
+    }
+
+    public function deleteExchange($name) {
+        
+    }
+
+    public function createQueue($name) {
+        
+    }
+
+    public function bindQueue($name) {
+        
+    }
+
+    public function deleteQueue($name) {
+        
+    }
+
+    public function deleteQueue($name) {
+        
+    }
+
+    public function consume($queue, $callback) {
+
+    }
+}
